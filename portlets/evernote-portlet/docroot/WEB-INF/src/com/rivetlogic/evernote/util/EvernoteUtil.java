ebdf0a2fbc23a74285563fd5872b2dc01bfaaf0e
jkamcc <juancamilocasas@gmail.com>
2014-05-12 09:54:05 -0600

    LRA-172 fix

--- portlets/evernote-portlet/docroot/WEB-INF/src/com/rivetlogic/evernote/util/EvernoteUtil.java
+++ portlets/evernote-portlet/docroot/WEB-INF/src/com/rivetlogic/evernote/util/EvernoteUtil.java
@@ -21,7 +21,6 @@ import static com.rivetlogic.evernote.util.EvernoteConstants.ACCESS_TOKEN;
 import static com.rivetlogic.evernote.util.EvernoteConstants.EVERNOTE_SERVICE;
 import static com.rivetlogic.evernote.util.EvernoteConstants.REQUEST_TOKEN;
 import static com.rivetlogic.evernote.util.EvernoteConstants.REQUEST_TOKEN_SECRET;
-import static com.rivetlogic.evernote.util.EvernoteConstants.INVALID_API_KEY_ERROR;
 import static com.rivetlogic.evernote.util.EvernoteConstants.OAUTH_VERIFIER;
 import static com.rivetlogic.evernote.util.EvernoteConstants.AUTHORIZATION_URL;
 
@@ -38,23 +37,19 @@ import org.scribe.oauth.OAuthService;
 import com.evernote.auth.EvernoteAuth;
 import com.evernote.auth.EvernoteService;
 import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.oauth.OAuthException;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.PortalUtil;
-import com.rivetlogic.evernote.exception.InvalidApiKeyException;
 import com.rivetlogic.evernote.portlet.EvernoteKeys;
 
 /**
  * @author dong-junkim
- *
  */
 public class EvernoteUtil {
-	private static Log LOG = LogFactoryUtil.getLog(EvernoteUtil.class);
 	
 	public static OAuthService getOAuthService(HttpServletRequest request, ThemeDisplay themeDisplay) 
-		throws SystemException, InvalidApiKeyException {
+		throws SystemException {
 		
 		OAuthService service = null;
 		String consumerKey = StringPool.BLANK; 
@@ -69,57 +64,57 @@ public class EvernoteUtil {
 	    				EvernoteApi.Sandbox.class;
 		// Get consumer key and consumer secret
         
-		try {
-			consumerKey = EvernoteKeys.getConsumerKey(themeDisplay.getCompanyId());
-			consumerSecret = EvernoteKeys.getConsumerSecret(themeDisplay.getCompanyId());
-			
-			service = new ServiceBuilder().provider(providerClass)
-							.apiKey(consumerKey).apiSecret(consumerSecret).callback(cbUrl)
-							.build();
+		consumerKey = EvernoteKeys.getConsumerKey(themeDisplay.getCompanyId());
+		consumerSecret = EvernoteKeys.getConsumerSecret(themeDisplay.getCompanyId());
+		
+		service = new ServiceBuilder().provider(providerClass)
+						.apiKey(consumerKey).apiSecret(consumerSecret).callback(cbUrl)
+						.build();
 	
-		} catch (IllegalArgumentException e) {
-			LOG.error(INVALID_API_KEY_ERROR);
-			throw new InvalidApiKeyException(INVALID_API_KEY_ERROR);
-		}
-
 		return service;
 	}
 	
 	public static void authenticateEvernote(RenderRequest renderRequest, PortletSession portletSession, ThemeDisplay themeDisplay) 
-		throws SystemException {
+		throws OAuthException {
 		
 		HttpServletRequest request = PortalUtil.getHttpServletRequest(renderRequest);
 		String authorizationUrl = StringPool.BLANK;
 		
-		OAuthService service = getOAuthService(request, themeDisplay);
-
-		if (PortalUtil.getOriginalServletRequest(request).getParameter(OAUTH_VERIFIER) == null) {
-			// Send an OAuth message to the Provider asking for a new Request
-			// Token because we don't have access to the current user's account.
-			Token scribeRequestToken = service.getRequestToken();  
-			 
-			portletSession.setAttribute(REQUEST_TOKEN, scribeRequestToken.getToken());
-			portletSession.setAttribute(REQUEST_TOKEN_SECRET, scribeRequestToken.getSecret());
+		try {
 			
-			authorizationUrl = EVERNOTE_SERVICE.getAuthorizationUrl(scribeRequestToken.getToken());
-			
-		} else {
-			// Send an OAuth message to the Provider asking to exchange the
-			// existing Request Token for an Access Token
-			Token scribeRequestToken = new Token(
-					portletSession.getAttribute(REQUEST_TOKEN).toString(), 
-					portletSession.getAttribute(REQUEST_TOKEN_SECRET).toString());
-			
-			Verifier scribeVerifier = new Verifier(
-					PortalUtil.getOriginalServletRequest(request).getParameter(OAUTH_VERIFIER));
-			
-			Token scribeAccessToken = service.getAccessToken(scribeRequestToken, scribeVerifier);
-			
-			EvernoteAuth evernoteAuth = EvernoteAuth.parseOAuthResponse(
-					EVERNOTE_SERVICE, scribeAccessToken.getRawResponse());
-			
-			portletSession.setAttribute(ACCESS_TOKEN, evernoteAuth.getToken());
-		}                 
+			OAuthService service = getOAuthService(request, themeDisplay);
+	
+			if (PortalUtil.getOriginalServletRequest(request).getParameter(OAUTH_VERIFIER) == null) {
+				// Send an OAuth message to the Provider asking for a new Request
+				// Token because we don't have access to the current user's account.
+				Token scribeRequestToken = service.getRequestToken();  
+				 
+				portletSession.setAttribute(REQUEST_TOKEN, scribeRequestToken.getToken());
+				portletSession.setAttribute(REQUEST_TOKEN_SECRET, scribeRequestToken.getSecret());
+				
+				authorizationUrl = EVERNOTE_SERVICE.getAuthorizationUrl(scribeRequestToken.getToken());
+				
+			} else {
+				// Send an OAuth message to the Provider asking to exchange the
+				// existing Request Token for an Access Token
+				Token scribeRequestToken = new Token(
+						portletSession.getAttribute(REQUEST_TOKEN).toString(), 
+						portletSession.getAttribute(REQUEST_TOKEN_SECRET).toString());
+				
+				Verifier scribeVerifier = new Verifier(
+						PortalUtil.getOriginalServletRequest(request).getParameter(OAUTH_VERIFIER));
+				
+				Token scribeAccessToken = service.getAccessToken(scribeRequestToken, scribeVerifier);
+				
+				EvernoteAuth evernoteAuth = EvernoteAuth.parseOAuthResponse(
+						EVERNOTE_SERVICE, scribeAccessToken.getRawResponse());
+				
+				portletSession.setAttribute(ACCESS_TOKEN, evernoteAuth.getToken());
+			}       
+		
+		} catch (Exception e) {
+			throw new OAuthException(e);
+		}
 		
 		renderRequest.setAttribute(AUTHORIZATION_URL, authorizationUrl);   
 	}
